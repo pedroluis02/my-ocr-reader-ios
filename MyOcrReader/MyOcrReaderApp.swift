35002a76f508c593c3a3a768ab1f3b2fce5a5f6c
Pedro Luis <pedroluisnr22@gmail.com>
2023-06-29 23:43:30 -0500

    Initial Commit

---
+++ MyOcrReader/MyOcrReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOcrReaderApp.swift
+//  MyOcrReader
+//
+//  Created by Pedro Luis on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MyOcrReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
