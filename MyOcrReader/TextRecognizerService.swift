a38e1baecdc152050c02aeb52c87dfb7cbb519bb
Pedro Luis <pedroluisnr22@gmail.com>
2023-07-09 00:17:52 -0500

    text recognizer protocol

---
+++ MyOcrReader/TextRecognizerService.swift
@@ -0,0 +1,12 @@
+//
+//  TextRecogizerService.swift
+//  MyOcrReader
+//
+//  Created by Pedro Luis on 8/07/23.
+//
+
+import CoreMedia
+
+protocol TextRecognizerService {
+    func recognize(_ data: Data, success: @escaping ([String]) -> Void, failure: @escaping (Error) -> Void)
+}
