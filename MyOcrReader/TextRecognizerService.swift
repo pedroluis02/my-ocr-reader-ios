d36170d76de326d6001730359ee321208754574b
Pedro Luis <pedroluisnr22@gmail.com>
2023-07-13 23:28:08 -0500

    text recognition from camera buffer image

--- MyOcrReader/TextRecognizerService.swift
+++ MyOcrReader/TextRecognizerService.swift
@@ -9,4 +9,5 @@ import CoreMedia
 
 protocol TextRecognizerService {
     func recognize(_ data: Data, success: @escaping ([String]) -> Void, failure: @escaping (Error) -> Void)
+    func recognize(_ buffer: CMSampleBuffer,_ isBackPosition: Bool, success: @escaping ([String]) -> Void, failure: @escaping (Error) -> Void)
 }
