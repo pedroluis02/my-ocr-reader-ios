41fe76654f6948e9355452046d720d2ec6378747
Pedro Luis <pedroluisnr22@gmail.com>
2023-07-13 23:24:58 -0500

    rotate device camera

--- MyOcrReader/CameraUIKit/CameraUIViewController.swift
+++ MyOcrReader/CameraUIKit/CameraUIViewController.swift
@@ -5,18 +5,19 @@
 //  Created by Pedro Luis on 11/07/23.
 //
 
-import Foundation
 import UIKit
 import AVFoundation
+import MLKit
 
 class CameraUIViewController : UIViewController {
     private var permissionGranted: Bool = false
+    private let devicePosition: AVCaptureDevice.Position = .back
     
     private let captureSession: AVCaptureSession = AVCaptureSession()
     private let sessionQueue = DispatchQueue(label: "sessionQueue")
     
     private var previewLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer()
-    var screenRect: CGRect! = nil
+    private var screenRect: CGRect! = nil
     
     override func viewDidLoad() {
         checkPermission()
@@ -27,12 +28,18 @@ class CameraUIViewController : UIViewController {
         }
     }
     
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        screenRect = UIScreen.main.bounds
+        previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
+        previewLayer.connection?.videoOrientation = currentAVOrientation()
+    }
+    
     private func setupCaptureSession() {
-        guard let videoDevice = AVCaptureDevice.default(.builtInDualWideCamera, for: .video, position: .back) else { return }
-        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice) else { return }
+        guard let captureDevice = AVCaptureDevice.default(.builtInDualWideCamera, for: .video, position: devicePosition) else { return }
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: captureDevice) else { return }
         
-        guard captureSession.canAddInput(videoDeviceInput) else { return }
-        captureSession.addInput(videoDeviceInput)
+        guard captureSession.canAddInput(captureDeviceInput) else { return }
+        captureSession.addInput(captureDeviceInput)
         
         screenRect = UIScreen.main.bounds
         
@@ -65,5 +72,27 @@ class CameraUIViewController : UIViewController {
             self.permissionGranted = permissionGranted
             self.sessionQueue.resume()
         }
+    }
+    
+    private func currentAVOrientation() -> AVCaptureVideoOrientation {
+        var orientation: AVCaptureVideoOrientation
+        switch UIDevice.current.orientation {
+        case UIDeviceOrientation.portraitUpsideDown:
+            orientation = .portraitUpsideDown
+            
+        case UIDeviceOrientation.landscapeLeft:
+            orientation = .landscapeRight
+            
+        case UIDeviceOrientation.landscapeRight:
+            orientation = .landscapeLeft
+            
+        case UIDeviceOrientation.portrait:
+            orientation = .portrait
+            
+        default:
+            orientation = .portrait
+        }
+        
+        return orientation
     }
 }
